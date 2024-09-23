6b3f63e0b12e128e76bf944a7d145bb7b687a6cb
Albin Varghese <albin8285@gmail.com>
2024-09-23 18:41:05 +0530

    chore: change api route prefix and add ping endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ping route
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
