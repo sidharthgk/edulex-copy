a251b7a82dc0fc866ec5d7ca0cf487b4a67e0965
Albin Varghese <albin8285@gmail.com>
2024-09-23 21:21:38 +0530

    fix: exclude api endpoints from csrf protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+        $middleware->validateCsrfTokens(except: [
+            'v1/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
