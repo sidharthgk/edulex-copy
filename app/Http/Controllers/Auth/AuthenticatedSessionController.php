57fd9e6b21a3abf2ff15c9d0d5c93aa8ea34164b
Albin Varghese <albin8285@gmail.com>
2024-09-24 00:35:34 +0530

    chore: add logout method

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -78,15 +78,12 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      *
-     * ❌❌❌❌
+     *
      */
     public function destroy(Request $request): Response
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
+        // remove old tokens
+        $request->user()->tokens->each->delete();
 
         return response()->noContent();
     }
--- routes/auth.php
+++ routes/auth.php
@@ -30,13 +30,13 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth:api', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['auth:api', 'throttle:6,1'])
                 ->name('verification.send');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+                ->middleware('auth:api')
                 ->name('logout');
