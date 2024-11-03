5d2ac020672546aee6ceb2f9c189faae0a0c7b52
Albin Varghese <albin8285@gmail.com>
2024-11-03 11:27:11 +0530

    chore: retrieve account balance from user profile

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api'])->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->load('wallet');
 });
 
 // ping route
